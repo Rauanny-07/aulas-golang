e58ab495eabeafc0187f3e8aaebc9951f63c3a55
Aluno Dve25 <rauanny.rodrigues@aluno.senai.br>
2025-04-10 10:45:33 -0300

    soma com numeros interiros

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
-fmt.Println("Digite 5 números inteiros:")
-     for i := 0; i < 5; i++ {
-       fmt.Printf("Número %d: ", i+1)
-       fmt.Scan(&numeros[i])
-       soma += numeros[i]
-     }
-     fmt.Printf("A soma dos números digitados é: %d\n", soma)
-   }
+
+var idade int
+
+	fmt.Print("Digite sua idade: ")
+	_, err := fmt.Scan(&idade)
+
+
+	if err != nil {
+		fmt.Println("Erro ao ler a idade. Certifique-se de digitar um número.")
+		return
+	}
+
+	
+	if idade < 18 {
+		fmt.Println("Você é menor de idade")
+	} else if idade <= 60 {
+		fmt.Println("Você é adulto")
+	} else {
+		fmt.Println("Você é idoso")
+	}
+
+
+ 
