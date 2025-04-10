76165e037056516c585dd6e2952193959fea1414
Aluno Dve25 <rauanny.rodrigues@aluno.senai.br>
2025-04-10 08:41:53 -0300

    soma com numeros interiros

--- main.go
+++ main.go
@@ -1,36 +1,9 @@
 package main
-   "fmt"
-   "strings"
-   "sort"
-
-func main(){
-    greeting := "hello my frinds!"
-	fmt.Println(strings.Contains(greeting,"dogs")) 
-	fmt.Println(strings.ReplaceAll(greeting,"my", "mine")) 
-	fmt.Println(strings.ToUpper(greeting)) 
-	fmt.Println(strings.Index(greeting,"my")) 
-	fmt.Println(strings.Split(greeting,"")) 
-	ages:= []int{50,80,10} 
-	sort.Ints(ages) 
-	fmt.Println(ages) 
-	Index:= sort.SearchInts(ages,50)  
-    fmt.Println(index) 
-    names :=[]string{"Alice", "marco", "Diego"} 
-    sort.Strings(names) 
-    fmt.Println(names) 
-    fmt.Println(sort.SearchStrings(nomes,"Alice"))
-
-	x <5
-
-	for i :=0; i <len(names); i++{
-		fmt.Println(names[i])
-	}
-
-	for index, value := range names {
-		fmt.Println("O Indice é", index, "e o valor", Value)
-	
-	}
-
-		for index, Value := range ages {
-			fmt.Println("O Indice é", index, "é o valor", Value)
-		}
+fmt.Println("Digite 5 números inteiros:")
+     for i := 0; i < 5; i++ {
+       fmt.Printf("Número %d: ", i+1)
+       fmt.Scan(&numeros[i])
+       soma += numeros[i]
+     }
+     fmt.Printf("A soma dos números digitados é: %d\n", soma)
+   }
