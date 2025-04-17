b54179c37d1fcf114d19c018e7367ec4d6568680
Aluno Dve25 <rauanny.rodrigues@aluno.senai.br>
2025-04-17 09:17:31 -0300

    operaçoes basicas

--- main.go
+++ main.go
@@ -1,24 +1,35 @@
 package main
 
-var idade int
+import "fmt"
 
-	fmt.Print("Digite sua idade: ")
-	_, err := fmt.Scan(&idade)
+func dividir(dividendo int, divisor int) (int, string) {
+  if divisor == 0 {
+    return 0, "Erro na divição"
+  }
+   return dividendo / divisor, "Sem erro"
 
-
-	if err != nil {
-		fmt.Println("Erro ao ler a idade. Certifique-se de digitar um número.")
-		return
-	}
-
-	
-	if idade < 18 {
-		fmt.Println("Você é menor de idade")
-	} else if idade <= 60 {
-		fmt.Println("Você é adulto")
-	} else {
-		fmt.Println("Você é idoso")
-	}
+}
 
 
- 
+  func operaçaoBasica(a int, b int) (int, int, int){
+      soma := a + b
+      multiplicaçao := a * b
+      subitraçao := a - b
+      return soma, multiplicaçao, subitraçao
+
+}
+
+func main (){
+  resultado, erro := dividir(10,2)
+
+    if erro != "Sem erro"{
+        fmt.Println(erro)
+}else {    
+      fmt.Println("O resultado da divisao é:", resultado , erro)
+}
+
+soma, mult, sub:= operaçaoBasica(10,2)
+  fmt.Println(soma)
+  fmt.Println(mult)
+  fmt.Println(sub)
+}
