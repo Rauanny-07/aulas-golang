3e06df552d5e1765077761fab70e7dca2ad8d9b4
Aluno Dve25 <rauanny.rodrigues@aluno.senai.br>
2025-04-17 10:22:15 -0300

    idade 18

--- main.go
+++ main.go
@@ -1,35 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-func dividir(dividendo int, divisor int) (int, string) {
-  if divisor == 0 {
-    return 0, "Erro na divição"
+)
+
+func dadosPessoais(idade int, nome string) (int, string){
+
+  if idade >= 18 {
+    return idade, "maior de idade"
+} else {
+     return idade,"menor de idade"
+}
+   
   }
-   return dividendo / divisor, "Sem erro"
 
-}
+  func main(){
 
+    var idade int
+    var nome string
 
-  func operaçaoBasica(a int, b int) (int, int, int){
-      soma := a + b
-      multiplicaçao := a * b
-      subitraçao := a - b
-      return soma, multiplicaçao, subitraçao
-
-}
-
-func main (){
-  resultado, erro := dividir(10,2)
-
-    if erro != "Sem erro"{
-        fmt.Println(erro)
-}else {    
-      fmt.Println("O resultado da divisao é:", resultado , erro)
-}
-
-soma, mult, sub:= operaçaoBasica(10,2)
-  fmt.Println(soma)
-  fmt.Println(mult)
-  fmt.Println(sub)
+  fmt.Println("Seu nome é:")
+   fmt.Scan(&nome)
+   fmt.Println("Sua idade é")
+   fmt.Scan(&idade)
+    
+  var nomep, idadep = dadosPessoais(idade, nome)
+fmt.Println(nomep)
+fmt.Println(idadep)
 }
