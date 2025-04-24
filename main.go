e954413af33d689a3c02b7420ff221ba60fd9033
Aluno Dve25 <rauanny.rodrigues@aluno.senai.br>
2025-04-24 13:28:45 -0300

    pegar nome

--- main.go
+++ main.go
@@ -2,20 +2,14 @@ package main
 
 import ("fmt")
 
-func main()
+func PegarNome() (string, string){
+      return "Barry", "Allen"
+}
 
-alunoIdade := make(map[string]int)
-alunoIdade ["Bruno"] =15
-alunoIdade ["Otavio"] = 16
-alunoIdade ["Fabio"] = 40
-alunoIdade ["Isabela"] = 15
-fmt.Println("idade do bruno", alunoIdade["Bruno"])
-notasAluno := map [string]float64{
-    "Bruno" := 10;
-    "Otavio" := 10;
-    "Fabio" := 8.7;
-    "Isabela" := 9.5;
-   for nome, nota := range notasAluno{
-    fmt.Println("%s tirou a nota %.1f \n", k, v)
-   }
+
+
+func main(){
+    nome, sobrenome := PegarNome()
+    fmt.Println(nome)
+    fmt.Println(sobrenome)
 }
